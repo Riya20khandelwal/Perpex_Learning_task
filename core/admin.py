e15f4554d5d423243ad1b092f87276100e87acf1
Riya Khandelwal <riyakhandelwal204@gmail.com>
2025-07-10 19:42:53 +0530

    Initial Commit

---
+++ core/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import Task
+
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'completed')
+    search_fields = ('title',)
+
+admin.site.register(Task, TaskAdmin)
